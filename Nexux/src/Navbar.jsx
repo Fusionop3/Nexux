c6b9c4014a3d88728594ec253c249e9ec85195ff
Fusionop3 <vineetkumar2823@outlook.com>
2025-08-22 11:29:42 +0530

    new commit

--- Nexux/src/Navbar.jsx
+++ Nexux/src/Navbar.jsx
@@ -6,15 +6,15 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">
-        {/* Placeholder for your logo */}
         <img src="https://placehold.co/40x40/white/white" alt="NEXUS" className="logo-icon" />
         <span className="logo-text">NEXUS</span>
       </div>
       <div className="navbar-links">
-        {/* We can add navigation links here later */}
+        <a href="#features" className="navbar-link">Features</a>
+        <a href="#pricing" className="navbar-link">Pricing</a>
+        <a href="#contact" className="navbar-link">Contact</a>
       </div>
       <div className="navbar-menu-icon">
-        {/* Hamburger icon using a simple SVG */}
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M4 6H20M4 12H20M4 18H20" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
