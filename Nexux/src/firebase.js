03868c021fe059dd5a02ce43cc6852f696c26310
Fusionop3 <vineetkumar2823@outlook.com>
2025-08-23 21:47:26 +0530

    after firebase

--- Nexux/src/firebase.js
+++ Nexux/src/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyAVJogMgc14Jbl53g_9DOHTK5ybBex7xXg",
+  authDomain: "nexux-825ac.firebaseapp.com",
+  projectId: "nexux-825ac",
+  storageBucket: "nexux-825ac.firebasestorage.app",
+  messagingSenderId: "959521816603",
+  appId: "1:959521816603:web:c649facb2324b8d8bff8be",
+  measurementId: "G-V5HF5DF863"
 };
 
 const app = initializeApp(firebaseConfig);
