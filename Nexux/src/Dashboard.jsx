96762767becb4c1a12008d40c32f9094d5e682a7
Fusionop3 <vineetkumar2823@outlook.com>
2025-08-25 23:01:36 +0530

    debugging

--- Nexux/src/Dashboard.jsx
+++ Nexux/src/Dashboard.jsx
@@ -40,7 +40,7 @@ const Dashboard = () => {
   const handleLogout = async () => {
     try {
       await logout();
-      window.location.href = "/auth.jsx"; // 👈 redirect to login
+      window.location.href = "app.jsx"; // 👈 redirect to login
     } catch (error) {
       console.error(error);
       alert("Logout failed.");
